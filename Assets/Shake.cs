4bc058d4e8d71a64d3dc5bba376693a743373fbb
shebkoch <shebkoch@gmail.com>
2017-08-01 16:56:54 +0500

    'init'

---
+++ Assets/Shake.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Shake : MonoBehaviour {
+
+	public Text score;
+	void Update () {
+		int sc = 0;
+		if (Input.acceleration.magnitude < 1f) sc++;
+		score.text = sc.ToString(); 
+	}
+}
