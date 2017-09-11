877b83b069529035d73b897c7186f302a9a2a5ab
Sheb <shebkoch@gmail.com>
2017-09-11 10:37:30 +0500

    test

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	public float speed = 10.0F;
+	void Update () {
+		Vector3 dir = Vector3.zero;
+		dir.x = -Input.acceleration.y;
+		dir.z = Input.acceleration.x;
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
+
+		dir *= Time.deltaTime;
+		transform.Translate(dir * speed);
+	}
+}
