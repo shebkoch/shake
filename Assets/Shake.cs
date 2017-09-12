f32b508dd52874b4af941f46b034fd3573eb2151
Sheb <shebkoch@gmail.com>
2017-09-12 10:38:10 +0500

    bottles swap

--- Assets/Shake.cs
+++ Assets/Shake.cs
@@ -7,23 +7,46 @@ public class Shake : MonoBehaviour {
 	public GameObject startMenu;
 	public GameObject cork;
 	public Image slider;
-	public float maxScore;
-	public bool isClicked = false;
+	public float maxScore;	
+	public GameObject[] bottles = new GameObject[3];
+	int bottlePointer = 0;
 	float magnitude;
-	void ClickTest()
+	public void LeftSwap()
 	{
-		isClicked = true;
+		bottlePointer--;
+		if (bottlePointer < 0) bottlePointer = bottles.Length- 1;
+		ShowBottle();
+	}
+	public void RightSwap()
+	{
+		bottlePointer++;
+		if (bottlePointer > bottles.Length - 1) bottlePointer = 0;
+		ShowBottle();
+	}
+	void ShowBottle()
+	{
+		foreach (var bottle in bottles)
+		{
+			Debug.Log(1);
+			bottle.SetActive(false);
+		}
+		
+		bottles[bottlePointer].SetActive(true);
+	}
+	void Start()
+	{
+		bottles = GameObject.FindGameObjectsWithTag("bottle");
+		ShowBottle();
 	}
 	void Update () {
 		//magnitude = Input.acceleration.magnitude;
 		magnitude = Input.acceleration.y;
 		Vector3 corkPos = cork.transform.position;
-		//if (magnitude >= 0.5) {
+		if (magnitude >= 0.5) {
 			startMenu.SetActive(false);
 			maxScore += magnitude;
-		if(isClicked) maxScore += 0.5f;
 			slider.fillAmount = maxScore / 250;
-		//}
+		}
 		if (maxScore >= 250)
 			cork.transform.position = new Vector3(corkPos.x, corkPos.y+0.5f,0);
 		//Debug.Log(maxScore);
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,21 +4,11 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public List<GameObject> toInit = new List<GameObject>();
+	void Start()
+	{
+		foreach (var i in toInit) Instantiate(i);
 	}
 
-	// Update is called once per frame
-	public float speed = 10.0F;
-	void Update () {
-		Vector3 dir = Vector3.zero;
-		dir.x = -Input.acceleration.y;
-		dir.z = Input.acceleration.x;
-		if (dir.sqrMagnitude > 1)
-			dir.Normalize();
-
-		dir *= Time.deltaTime;
-		transform.Translate(dir * speed);
-	}
+	
 }
