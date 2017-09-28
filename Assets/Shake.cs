a6dc51b6d30e6ba9940fe7b9b906d39bcc839870
shebkoch <shebkoch@gmail.com>
2017-09-28 08:07:20 +0500

    fix wrong folderwith project

--- Assets/Shake.cs
+++ Assets/Shake.cs
@@ -5,55 +5,38 @@ using UnityEngine.UI;
 public class Shake : MonoBehaviour {
 
 	public GameObject startMenu;
+	public GameObject endMenu;
+	Swap swap;
 	Transform cap;
 	public Image slider;
 	public float maxScore;
-	const int bottleCount = 4;
-	public GameObject[] bottles = new GameObject[bottleCount];
-	int bottlePointer = 0;
-	float magnitude;
-	public void LeftSwap()
-	{
-		bottlePointer--;
-		if (bottlePointer < 0) bottlePointer = bottles.Length- 1;
-		ShowBottle();
+	bool isEnd = false;
+	float magnitude = 0;
+	public static Shake Instance { get; private set; }
+	public void Awake() {
+		Instance = this;
 	}
-	public void RightSwap()
-	{
-		bottlePointer++;
-		if (bottlePointer > bottles.Length - 1) bottlePointer = 0;
-		ShowBottle();
-	}
-	void ShowBottle()
-	{
-		foreach (var bottle in bottles)
-		{
-			Debug.Log(1);
-			bottle.SetActive(false);
-		}
-		
-		bottles[bottlePointer].SetActive(true);
-	}
+	
 	void Start()
 	{
-		bottles = GameObject.FindGameObjectsWithTag("bottle");
-		ShowBottle();
+		swap = Swap.Instance;
 	}
 	void Update () {
-		//magnitude = Input.acceleration.magnitude;
-		magnitude = Input.acceleration.y;
-		cap = bottles[bottlePointer].transform.GetChild(0);
-		if (magnitude >= 0.5) {
+		magnitude = Mathf.Abs(Input.acceleration.y - magnitude);
+		cap = swap.GetCap();
+		if (magnitude >= 0.5 && swap.IsBought()) { //TODO: add ui
 			startMenu.SetActive(false);
 			maxScore += magnitude;
 			slider.fillAmount = maxScore / 250;
 		}
 		if (maxScore >= 250)
 		{
-			
+			if (!isEnd) {
+				endMenu.SetActive(true);
+				EconomicsControl.Instance.RewardGain();
+				isEnd = true;
+			}
 			cap.position = new Vector3(cap.position.x, cap.position.y + 0.5f, 0);
 		}
-		//Debug.Log(maxScore);
-		//Debug.Log("sli" + slider.fillAmount);
 	}
 }
